fdff330368538e18f5d81c3454d4c1827eeb0552
Iliyan0411 <ilian.dd00@gmail.com>
2022-03-21 23:43:14 +0200

    Seminar 4, theoretical part

---
+++ Seminar 4/sem_4.sql
@@ -0,0 +1,28 @@
+﻿use ships;
+
+select country, name
+from classes
+join ships on classes.class = ships.class
+left join outcomes on name = ship
+where outcomes.ship is null;
+
+select distinct classes.class, battle
+from classes
+left join ships on classes.class = ships.class
+join outcomes on name = ship
+where country = 'Gt.Britain';
+
+--За всеки клас да се изведат името му, държавата и имената 
+--на всички негови кораби, пуснати през 1916 г.
+select classes.class, country, name
+from classes
+join ships on classes.class = ships.class
+where launched = 1916;
+
+
+--За всеки клас да се изведат името му, държавата и имената 
+--на всички негови кораби, пуснати през 1916 г. Ако за някой клас няма кораб пуснат през 1916г., 
+-- срещу тях да пише NULL
+select classes.class, country, name
+from classes
+left join ships on classes.class = ships.class and launched = 1916;
