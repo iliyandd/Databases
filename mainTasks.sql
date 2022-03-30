a91d33e0ad9f6f6ba5876054850eb713ff4f45c4
Iliyan0411 <ilian.dd00@gmail.com>
2022-03-30 13:55:03 +0300

    All tasks are solved here

--- mainTasks.sql
+++ mainTasks.sql
@@ -36,3 +36,50 @@ left join ships on classes.class = ships.class
 where name is null or classes.class not in (select class
 											from ships
 											where year(launched) > 1921);
+
+
+use movies;
+
+--Всички филми, чието заглавие съдържа едновременно думите 'Star' и 'Trek' (не
+--непременно в този ред). Резултатите да се подредят по година (първо най-новите
+--филми), а филмите от една и съща година - по азбучен ред
+select *
+from movie
+where title like '%Star%' and title like '%Trek%'
+order by year desc, title;
+
+
+--Заглавията и годините на филмите, в които са играли звезди, родени между
+--1.1.1970 и 1.7.1980.
+select movietitle, movieyear
+from starsin
+join moviestar on starname = name
+where birthdate >= '1970-01-01' and birthdate <= '1980-07-01';
+
+
+use pc;
+
+--Компютрите, които са по-евтини от всеки лаптоп на същия производител.
+select pc.*
+from pc
+join product p1 on pc.model = p1.model
+where price > all(select price
+					 from laptop
+					 join product p2 on laptop.model = p2.model
+					 where p1.maker = p2.maker);
+
+
+--Компютрите, които са по-евтини от всеки лаптоп и принтер на същия
+--производител.
+select pc.*
+from pc
+join product p1 on pc.model = p1.model
+where price < all (select price
+					from laptop
+					join product p2 on laptop.model = p2.model
+					where p1.maker = p2.maker)
+	and price < all (select price
+					from printer
+					join product p2 on printer.model = p2.model
+					where p1.maker = p2.maker);
+
