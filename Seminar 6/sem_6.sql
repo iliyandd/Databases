c5a2727c86c76b40ee2471f193145c5f03bfe046
Iliyan0411 <ilian.dd00@gmail.com>
2022-04-04 00:50:20 +0300

    First task of Seminar 6

---
+++ Seminar 6/sem_6.sql
@@ -0,0 +1,12 @@
+﻿--Без повторение заглавията и годините на всички филми, заснети преди 1982, в които е играл
+--поне един актьор (актриса), чието име не съдържа нито буквата 'k', нито 'b'. Първо да се изведат
+--най-старите филми
+use movies;
+
+select distinct title, year
+from movie
+join starsin on title = movietitle and year = movieyear
+where movieyear = 1982 and starname not in (select starname
+											from starsin
+											where starname like '%b%' and starname like '%k%')
+order by year;
