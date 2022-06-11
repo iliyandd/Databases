f745e18f2c43a289305e88cea4be7f249a378675
Iliyan0411 <ilian.dd00@gmail.com>
2022-06-11 19:00:21 +0300

    Past exams - 2018, 2019

---
+++ july_2018.sql
@@ -0,0 +1,26 @@
+﻿use movies;
+
+
+/* (13.07.2013) Задача 4.1. Да се напише заявка, която да изведе име на студио, 
+година на първия филм за това студио, година на последния филм за това студио и броя
+на всички филми за това студио, само за тези студиа започващи с буквата 'M'.*/
+
+select studioname, min(year), max(year), count(*)
+from movie
+where studioname like 'M%'
+group by studioname;
+
+
+/* (13.07.2013) Задача 4.2. Да се напише заявка, която да изведе името на актрисата,
+участвала в най-много филми, и броя на филмите, в които е участвала.*/
+
+select starname, count(movietitle)
+from starsin
+join moviestar on starname = name
+where gender = 'F'
+group by starname
+having count(movietitle) >= all(select count(*)
+								from starsin
+								join moviestar on starname = name
+								where gender = 'F'
+								group by starname);
---
+++ july_2019.sql
@@ -0,0 +1,10 @@
+﻿use movies;
+
+
+/* (09.07.2019) Задача 4.2. Да се напише заявка,която да изведе имената на всички
+продуценти с минимален нетен актив.*/
+
+select distinct name
+from movieexec
+where networth <= all(select networth
+					  from movieexec);
