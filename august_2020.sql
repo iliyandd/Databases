090d4e7a5c734a69603fb728b15f8dab80251f28
Iliyan0411 <ilian.dd00@gmail.com>
2022-06-11 18:15:03 +0300

    Past exams 2017, 2020

---
+++ august_2020.sql
@@ -0,0 +1,31 @@
+﻿use movies;
+
+/* (05.08.2020) Задача 4.1. Да се напише заявка, която извежда имената и рождените дати на
+всички филмови звезди, чието име не съдържа "Ja" и са играли в поне един цветен филм.
+Първо да се изведат най-младите звезди, а звездите родени на една и съща дата, да се 
+изведат по азбучен ред.*/
+
+select distinct name, birthdate
+from movie
+join starsin on title = movietitle and year = movieyear
+join moviestar on name = starname
+where incolor = 'Y' and not name like '%Ja%'
+order by birthdate, name;
+
+
+/*(05.08.2020) Задача 4.2. Да се напише заявка, която извежда следната информация 
+за всяка актриса, играла в най-много шест филма:
+-име;
+-рождена година(напр. ако актрисата е родена на 1.1.1995 г., в колоната да пише 1995);
+-брой различни студиа, с които е работила.
+Ако за дадена актриса няма информация в какви филми е играла, за нея също да се изведе
+ред с горната информация, като за брой студиа се изведе 0.*/
+
+select name, year(birthdate), count(studioname)
+from moviestar
+left join starsin on name = starname
+left join movie on title = movietitle and year = movieyear
+where gender = 'F'
+group by name
+having count(starname) <= 6;
+
---
+++ july_2017.sql
@@ -0,0 +1,53 @@
+﻿use movies;
+
+/* (11.07.2017) Задача 3.1. Попълнете липсващите части, обозначени с ___ така, че 
+заявката да изведе за всяко студио името на студиото, заглавието и годината на филма,
+излязъл последно на екран за това студио.
+select studioname, title, year
+from movie m 
+where year = (select _____
+			  from movie
+              where _______________);*/
+
+select studioname, title, year
+from movie m
+where year = (select max(year)
+			  from movie
+			  where studioname = m.studioname);
+
+
+/* (11.07.2017) Задача 3.2. Попълнете липсващите части, обозначени с ___ така, че 
+заявката да изведе име на продуцент и обща дължина на продуцираните от него филми,
+за тези продуценти, които имат поне един филм преди 1980.
+select name, __________
+from movieexec join movie on producerc#=cert#
+____________________
+____________________;*/
+
+select name, sum(length)
+from movieexec
+join movie on producerc# = cert#
+where cert# in (select producerc#
+				from movie
+				where year < 1980)
+group by name;
+
+
+/* (11.07.2017) Задача 3.3. Попълнете липсващите части, обозначени с ___ така, че 
+заявкада да изведе име на актьорите, участвали във филми на продуценти с най-големи
+нетни активи, както и заглавие на филмите, в които са участвали, име на продуцент и
+нетни активи.
+select starname,title,name,networth
+from starsin join movie on movietitle=title and movieyear=year
+join (select cert#, networth, name
+	  from movieexec
+      where _______________)t
+______________________________;*/
+
+select starname, title, name, networth
+from starsin
+join movie on movietitle = title and movieyear = year
+join (select cert#, networth, name
+	  from movieexec
+	  where networth = (select max(networth)
+						from movieexec)) t on producerc# = t.cert#;
